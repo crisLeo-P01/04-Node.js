3ccff1e15c7f8b72b4ec89bed58800a6fa92cfc7
crisLeo <cristianleopaiva@gmail.com>
2021-11-02 00:09:21 -0300

    Consola interactiva

---
+++ 03-consola-interactiva/app.js
@@ -0,0 +1,15 @@
+require('colors');
+
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+console.clear();
+
+const main = async () => {
+
+  mostrarMenu();
+
+  //pausa();
+
+}
+
+main();
---
+++ 03-consola-interactiva/helpers/mensajes.js
@@ -0,0 +1,50 @@
+require('colors');
+
+const mostrarMenu = () => {
+
+  console.clear();
+  console.log('======================='.green);
+  console.log(' Seleccione una opción '.green);
+  console.log('=======================\n'.green);
+
+  console.log(`${'1.'.green} Crear tarea`);
+  console.log(`${'2.'.green} Listar tarea`);
+  console.log(`${'3.'.green} Listar tareas completadas`);
+  console.log(`${'4.'.green} Listar tareas pendientes`);
+  console.log(`${'5.'.green} Completar tarea(s)`);
+  console.log(`${'6.'.green} Borrar tarea`);
+  console.log(`${'0.'.green} Salir\n`);
+
+  // Recibir información del usuario
+  const readline = require('readline').createInterface ({
+    input: process.stdin, // Pausar la interfaz y esperar al usuario de haber ingresado y haber dado enter
+    output: process.stdout, // Mostrar en consola un mensaje cuando yo le estoy pidiendo algo al usuario
+  });
+
+  // Hora de utilizar la interface
+  readline.question('Selecciones una opción ', (opt) => {
+    // console.log({opt}); De esta forma se muestra en consola la opción seleccionada
+    readline.close();
+  });
+
+}
+
+// pausa de la aplciación
+const pausa = () => {
+
+  const readline = require('readline').createInterface ({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  // Hora de utilizar la interface
+  readline.question(`\nPresione ${'ENTER'.green} para continuar\n`, (opt) => {
+    readline.close();
+  });
+  
+}
+
+module.exports = {
+  mostrarMenu,
+  pausa
+}
