d860127dec5caa859cb023859748d26c806e5741
Cristian Paiva <cristianleopaiva@gmail.com>
2023-08-10 19:55:39 -0300

    Buffer

---
+++ Fundamentos Nodejs/04-memoria/01-buffer.js
@@ -0,0 +1,19 @@
+let buffer = Buffer.alloc(4); //> Estamos guardando la cantidad de espacio en memoria
+let bufferFrom = Buffer.from([1, 2, 5]); //> Guardando un array de numeros
+let bufferFromString = Buffer.from('Hola mundo'); //> Guardando una cadena de texto
+
+console.log(buffer);
+console.log(bufferFrom);
+console.log(bufferFromString); //> Es la versión cruda en la que te muestra el resultado
+console.log(bufferFromString.toString());
+
+//> Recorrido de abcedario
+let abc = Buffer.alloc(26);
+console.log(abc);
+
+for(let i = 0; i < 26; i++) {
+  abc[i] = i + 97;
+}
+
+console.log(abc);
+console.log(abc.toString());
