d01f3723f09c0ec0024a23f2f836d75e95f7f7c3
Cristian Paiva <cristianleopaiva@gmail.com>
2023-08-14 21:04:37 -0300

    Nueva ruta con Express

---
+++ nodeExpress/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const  routerApi = require('./routes')
+
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hola, mi server en express');
+});
+
+app.listen(port, () => {
+  console.log('Mi port ' + port);
+});
+
+routerApi(app);
