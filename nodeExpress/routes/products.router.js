ca98a5f9ad3bf94b2dc9c12d84043aa440bb4a73
Cristian Paiva <cristianleopaiva@gmail.com>
2023-08-15 20:07:45 -0300

    HTTP response status

--- nodeExpress/routes/products.router.js
+++ nodeExpress/routes/products.router.js
@@ -3,6 +3,7 @@ const { faker } = require('@faker-js/faker');
 
 const router = express.Router();
 
+//> GET ----------
 router.get('/', (req, res) => {
   const products = [];
   const {size} = req.query;
@@ -23,22 +24,30 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const {id} = req.params;
-  res.json({
-    id,
-    name: faker.commerce.productName(),
-    price: parseInt(faker.commerce.price(), 10),
-    image: faker.image.imageUrl(),
-  })
+  if(id >= '999') {
+    res.status(404).json({
+      message: 'Not Found'
+    })
+  } else {
+    res.status(200).json({
+      id,
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    })
+  }
 });
 
+//> POST  ----------
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body
   })
 });
 
+//> PATH  ----------
 router.patch('/:id', (req, res) => {
   const {id} = req.params;
   const body = req.body;
@@ -49,6 +58,8 @@ router.patch('/:id', (req, res) => {
   })
 });
 
+
+//> DELETE  ----------
 router.delete('/:id', (req, res) => {
   const {id} = req.params;
   res.json({
