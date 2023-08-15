fef87a35c08fcb67ade71c56e368d302579aaaa7
Cristian Paiva <cristianleopaiva@gmail.com>
2023-08-15 08:49:04 -0300

    Creando routes de users

--- nodeExpress/routes/users.router.js
+++ nodeExpress/routes/users.router.js
@@ -1,6 +1,35 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
 
 const router = express.Router();
+
+router.use('/', (req, res) => {
+  const persons = [];
+  const {size} = req.query;
+  const limit = size || 10;
+
+  for(let i = 0; i < limit; i++) {
+    persons.push({
+      name: faker.person.fullName(),
+      sex: faker.person.sex(),
+      jobArea: faker.person.jobArea(),
+      Image: faker.image.avatar()
+    })
+  }
+
+  res.json(persons);
+})
+
+router.use('/:id', (req, res) => {
+  const {id} = req.params;
+  res.json({
+    id,
+    name: faker.person.fullName(),
+    sex: faker.person.sex(),
+    jobArea: faker.person.jobArea(),
+    Image: faker.image.avatar()
+  })
+})
 
 router.get('/', (req, res) => {
   const {limit, offset} = req.query;
@@ -14,3 +43,4 @@ router.get('/', (req, res) => {
   }
 })
 
+module.exports = router;
