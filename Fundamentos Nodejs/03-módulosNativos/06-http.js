06b19efc320f31fe898aebabc77d5be2d0543202
Cristian Paiva <38576658+crisLeo-P01@users.noreply.github.com>
2024-08-26 12:47:35 -0300

    Update

--- Fundamentos Nodejs/03-módulosNativos/06-http.js
+++ Fundamentos Nodejs/03-módulosNativos/06-http.js
@@ -1,50 +1,53 @@
-const http = require('node:http')
+const http = require("node:http");
 
 /* El http tiene una función con dos parametros, request y response.
 Una petición desde un cliente hacia un servidor es una request,
 en la que tendrá cabeceras, métodos, etc.. y el servidor mandará
 una respuesta al cliente con esa información /// */
 
-http.createServer(function (req, res) {
-  console.log('Nueva petición!');
+http
+  .createServer(function (req, res) {
+    console.log("Nueva petición!");
+    console.log(req.url);
+
+    //> Cabecera
+    res.writeHead(201, { "Content-Type": "text/plain" });
+
+    //> Escribir respuesta al usuario
+    res.write("Hola, ya se usar HTTP de NodeJS");
+
+    res.end();
+  })
+  .listen(3000);
+
+http.createServer(router).listen(3000);
+function router(req, res) {
+  console.log("Nueva petición!");
   console.log(req.url);
 
-  //> Cabecera
-  res.writeHead(201, { 'Content-Type': 'text/plain' })
-
-  //> Escribir respuesta al usuario
-  res.write('Hola, ya se usar HTTP de NodeJS')
-
-  res.end();
-}).listen(3000)
-
-http.createServer(router).listen(3000)
-function router(req, res) {
-  console.log('Nueva petición!')
-  console.log(req.url)
-
   switch (req.url) {
-    case '/hola':
-      res.writeHead(201, { 'Content-Type': 'text/plain' })
-      res.write('Hola, que tal')
-      res.end()
-      break
+    case "/hola":
+      res.writeHead(201, { "Content-Type": "text/plain" });
+      res.write("Hola, que tal");
+      res.end();
+      break;
 
     default:
-      res.write('Error 404: No se lo que quieres')
-      res.end()
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.write("Error 404: No se lo que quieres");
+      res.end();
   }
 
-  res.end()
+  res.end();
 }
 
-console.log('Escuchando http en el puerto 3000')
+console.log("Escuchando http en el puerto 3000");
 
 const server = http.createServer((req, res) => {
-  console.log('request received')
-  res.end('Hola Mundo')
-})
+  console.log("request received");
+  res.end("Hola Mundo");
+});
 
 server.listen(0, () => {
-  console.log(`server listening on port http://localhost:${server.address().port}`)
-})
+  console.log(`server listening on port http://localhost:${server.address().port}`);
+});
